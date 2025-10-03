e3d876f842c4d3b55467827687e1fc4ad3634b3f
Pifagorr <152813877+FeHuX0@users.noreply.github.com>
2025-10-03 15:24:54 -0400

    Add files via upload

---
+++ lab1.2/main.py
@@ -0,0 +1,10 @@
+from ui.PostMachineUI import PostMachineUI
+
+def main():
+    print("=== Машина Поста ===")
+    print("Добро пожаловать!\n")
+    ui = PostMachineUI()
+    ui.main_menu()
+
+if __name__ == "__main__":
+    main()
