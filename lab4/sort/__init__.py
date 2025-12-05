7e940f7a7a44156151b4dbd862338baebb34fa06
Pifagorr <152813877+FeHuX0@users.noreply.github.com>
2025-12-05 23:00:04 +0300

    Delete lab4 directory

--- lab4/sort/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Sorting algorithms package with generic-friendly helpers."""
-
-from .Smoothsort import Smoothsort
-from .SimplePancakeSort import SimplePancakeSort
-from .Person import Person
-from .SortDemo import SortDemo
-
-__all__ = ["Smoothsort", "SimplePancakeSort", "Person", "SortDemo"]
